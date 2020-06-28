df2e5e2939cf51b0eca8720cc9172ae3cad37cda
Kamaljeet Khanna <kamaljeet.khanna.01@gmail.com>
2020-06-28 15:24:15 +0530

    Add POM file and App main class

---
+++ src/main/java/com/wednesday/CabBookingBootApp.java
@@ -0,0 +1,13 @@
+package com.wednesday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabBookingBootApp
+{
+  public static void main(String[]args)
+  {
+      SpringApplication.run(CabBookingBootApp.class,args);
+  }
+}
