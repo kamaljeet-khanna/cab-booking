df8e4a9dd50ef9b04249472341818f74eff39d59
Kamaljeet Khanna <kamaljeet.khanna.01@gmail.com>
2020-06-28 15:27:01 +0530

    Formatting changes

--- src/main/java/com/wednesday/CabBookingBootApp.java
+++ src/main/java/com/wednesday/CabBookingBootApp.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CabBookingBootApp
 {
-  public static void main(String[]args)
+  public static void main(String[] args)
   {
-      SpringApplication.run(CabBookingBootApp.class,args);
+      SpringApplication.run(CabBookingBootApp.class, args);
   }
 }
